d47d90681cb3c708bd7d1fb64507e576b3464050
Brandon <calabresebrandon3@gmail.com>
2017-10-13 10:28:21 -0500

    Everything almost works now, sort of

--- main.py
+++ main.py
@@ -1,52 +1,29 @@
-import sys 
-import urllib.request #url library opener
+from tropeWrapper import getLinkList
+from tropeWrapper import getTropeList
+from tropeWrapper import getGameDict
 import string #string operations
-import re #regex
-#import requests
-from bs4 import BeautifulSoup
-from prettytable import PrettyTable
 
 
 searchLink = 'https://google.com/search?gbv=1&q=tvtropes.org+'
 #searchLink = 'http://tvtropes.org/pmwiki/search_result.php?q='
 gameFound = False
 while gameFound == False : #Loop input until user finds the game they were looking for
-    linkList = []
-    table1 = PrettyTable(['Number','Game Name'])
     print('Enter the name of a Game, and similar games will be found: ')
-    sys.stdout.flush()
-
     inputText = input() #Get user input for game
     #inputText = "zelda 1"
     inputText = inputText.replace(' ','+') #Replace spaces with + for search
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    page = opener.open(searchLink + inputText)
-    soup = BeautifulSoup(page, 'html.parser')
-    #linkList = soup.findAll("a", href=lambda href: href and "VideoGame" in href)
-    for link in soup.findAll('a') :
-        print(link['href'])
-        if u'tvtropes' in link['href'] :
-            temp = link['href']
-            temp = temp[temp.find('http://tvtropes'):temp.find('&')+1]
-            temp = temp[temp.find('http://tvtropes'):temp.find('%')]
-            if 'videogame' in temp.lower() and temp not in linkList :
-                linkList.append(temp)
+    linkList = getLinkList(searchLink+inputText)
     if len(linkList) == 0:
         print('No results found, try again')
         continue
-    for i in range(len(linkList)) :
-        #print(linkList[i][48:])
-        temp = str(linkList[i][48:])
-        temp = re.sub(r"(?<=\w)([1-9A-Z])", r" \1", temp)
-        #print(type(temp))
-        table1.add_row([(i+1),temp])
-    print (table1)
-    print ('Enter the number of the game you want, else input will be regotten')
-    sys.stdout.flush()
+    print ('Enter the number of the game you want, anything else to re-input')
     gameSelect = input()
     if (0 <= int(gameSelect)-1 <= len(linkList)-1) :
         gameLink = linkList[int(gameSelect)-1]
         print(gameLink)
         gameFound = True
 
+tropeList = getTropeList(gameLink)
+gameList = getGameDict(tropeList,gameLink[48:])
+for i in gameList:
+    print(i)
---
+++ tropeWrapper.py
@@ -0,0 +1,79 @@
+import urllib.request #url library opener
+import re #regex
+import string
+import time
+import operator
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+
+
+def getLinkList(link):
+    linkList = []
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    page = opener.open(link)
+    soup = BeautifulSoup(page, 'html.parser')
+    for link in soup.findAll('a') :
+        if u'tvtropes' in link['href'] :
+            temp = link['href']
+            temp = temp[temp.find('http://tvtropes'):temp.find('&')+1]
+            temp = temp[temp.find('http://tvtropes'):temp.find('%')]
+            if '.php/videogame' in temp.lower() and temp not in linkList :
+                linkList.append(temp)
+    printTable(linkList)
+    return linkList
+
+def printTable(linkList):
+    table1 = PrettyTable(['Number','Game Name'])    
+    for i in range(len(linkList)) :
+        temp = str(linkList[i][48:])
+        temp = re.sub(r"(?<=\w)([1-9A-Z])", r" \1", temp)
+        table1.add_row([(i+1),temp])
+    print (table1)
+    
+def printTable2(gameList):
+    table1 = PrettyTable(['Number','Game Name'])
+    j = 1
+    for i in gameList :
+        temp = i[0]
+        temp = re.sub(r"(?<=\w)([1-9A-Z])", r" \1", temp)
+        table1.add_row([(j),temp])
+        if j == 10 :
+            break
+        j+=1
+    print (table1)
+    
+def getTropeList(gameLink):
+    tropeList = []
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    page = opener.open(gameLink)
+    soup = BeautifulSoup(page, 'html.parser')
+    for link in soup.findAll('a') :
+        if 'tvtropes.org' in link['href'] and 'homepage' not in link['href'] : 
+            temp = link['href']
+            if '.php/main' in temp.lower() and temp not in tropeList :
+                    tropeList.append(temp)
+    return tropeList
+
+def getGameDict(tropeList,gameName):
+    gameDict = {}
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    for i in tropeList:
+        page = opener.open(i)
+        soup = BeautifulSoup(page, 'html.parser')
+        for link in soup.findAll('a') :
+            if 'tvtropes.org' in link['href'] and 'homepage' not in link['href'] :
+                temp = link['href']
+                if '.php/videogame' in temp.lower() and gameName not in temp :
+                    if temp[48:] in gameDict :
+                        gameDict[temp[48:]] += 1
+                    else :
+                        gameDict[temp[48:]] = 1
+        time.sleep(0.5)
+    sortedDict = sorted(gameDict.items(), key=operator.itemgetter(1), reverse=True)
+    #for i in sortedDict :
+    #    print(i[0])
+    printTable2(sortedDict)
+    return sortedDict
